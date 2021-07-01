bd6fc3586708cf6585e1ab60228a1eccfac45248
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-01 19:29:06 +0300

    Feat(calculator): import calculate

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import ButtonPanelComponent from "./button-panel.component";
 import DisplayComponent from "./display.component";
 import React from "react";
+// eslint-disable-next-line no-unused-vars
+import calculate from '../logic/calculate';
 
 const App = () => {
   return (
