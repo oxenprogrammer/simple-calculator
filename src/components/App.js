764e42a443baef1daaddb17ac11ef76ba872c453
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-01 19:29:41 +0300

    Feat(calculator): finish milestone-4

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import ButtonPanelComponent from "./button-panel.component";
 import DisplayComponent from "./display.component";
 import React from "react";
 // eslint-disable-next-line no-unused-vars
-import calculate from '../logic/calculate';
+import calculate from "../logic/calculate";
 
 const App = () => {
   return (
