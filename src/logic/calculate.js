537a1c059226e37f42953444b3d448eaf4570d9d
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-02 01:24:40 +0300

    fixup! fixup! Feat(calculator): finish milestone-4

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,8 +2,8 @@ import operate from "./operate";
 
 const calculate = (calculator = {}, buttonName) => {
   let { total, next, operation } = calculator;
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const operators = ['+', '-', 'X', '÷', '%'];
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const operators = ["+", "-", "X", "÷", "%"];
 
   if (buttonName === "+/-") {
     return {
@@ -13,16 +13,16 @@ const calculate = (calculator = {}, buttonName) => {
     };
   }
 
-  if (buttonName === 'AC') {
+  if (buttonName === "AC") {
     return {
       ...calculator,
       total: null,
       next: null,
-      operation: null
-    }
+      operation: null,
+    };
   }
 
-  if (buttonName === '.') {
+  if (buttonName === ".") {
     if (operation === null) {
       return {
         ...calculator,
@@ -37,7 +37,7 @@ const calculate = (calculator = {}, buttonName) => {
 
   if (numbers.includes(buttonName)) {
     while (next === null) {
-      next = '';
+      next = "";
     }
     next += buttonName;
   }
@@ -58,7 +58,7 @@ const calculate = (calculator = {}, buttonName) => {
     }
   }
 
-  if (buttonName === '=') {
+  if (buttonName === "=") {
     const result = operate(total, next, operation);
     return {
       ...calculator,
@@ -68,12 +68,12 @@ const calculate = (calculator = {}, buttonName) => {
     };
   }
 
-  if (buttonName === '%') {
+  if (buttonName === "%") {
     return {
       ...calculator,
       total: total / 100,
       next: next / 100,
-    }
+    };
   }
 
   return { total, next, operation };
