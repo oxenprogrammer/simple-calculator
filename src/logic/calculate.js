87fb5ce3144e599b0efd75e46b23c1242535c463
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-01 18:40:47 +0300

    Feat(calculator): add calculate

---
+++ src/logic/calculate.js
@@ -0,0 +1,15 @@
+const calculate = (calculator={}, buttonName) => {
+  let { total, next, operation } = calculator;
+  
+  if (buttonName === '+/-') {
+    return {
+      ...calculator,
+      total: total * -1,
+      next: next * -1,
+    }
+  }
+
+  return { total, next, operation };
+}
+
+export default calculate;
