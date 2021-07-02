fa2bb8a89f8c63215db9b8793c1fcb0f14cbe07a
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-03 00:55:54 +0300

    Feat(calculator): add milestone 3

--- src/components/button-panel.component.js
+++ src/components/button-panel.component.js
@@ -1,7 +1,22 @@
 import ButtonComponent from "./button.component";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "grid",
+    gridTemplateColumns: "repeat(4, auto)",
+    gridGap: 2,
+    // flexWrap: "nowrap",
+    // height: 50,
+    // width: 50,
+    // border: "1px solid red",
+    // display: "flex",
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+}));
 
 const ButtonPanelComponent = ({ clickHandler }) => {
   const buttonNames = [
@@ -26,8 +41,9 @@ const ButtonPanelComponent = ({ clickHandler }) => {
     "=",
   ];
 
+  const classes = useStyles();
   return (
-    <React.Fragment>
+    <Grid container className={classes.root}>
       {buttonNames.map((buttonName) => (
         <ButtonComponent
           clickHandler={clickHandler}
@@ -35,7 +51,7 @@ const ButtonPanelComponent = ({ clickHandler }) => {
           key={buttonName}
         />
       ))}
-    </React.Fragment>
+    </Grid>
   );
 };
 
