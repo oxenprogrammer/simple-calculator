21b00284f53e0c792474119df17990f95e7f6fff
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-02 20:33:18 +0300

    Perf(config): add material icons

--- src/components/button-panel.component.js
+++ src/components/button-panel.component.js
@@ -1,5 +1,7 @@
 import ButtonComponent from "./button.component";
+import Grid from '@material-ui/core/Grid';
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
 
 const ButtonPanelComponent = ({ clickHandler }) => {
   const buttonNames = [
