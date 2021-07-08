253767d5f772becd3a76eff8efe5a30000b2e4cc
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-08 17:53:06 +0300

    Feat(math-web): add card

--- src/components/quote.component.js
+++ src/components/quote.component.js
@@ -1,9 +1,22 @@
-import qoute from '../assets/img/qoute.jpg';
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import qoute from "../assets/img/qoute.jpg";
+
+const useStyles = makeStyles(() => ({
+  card: {
+    "@media(max-width: 480px)": {
+      width: "90%",
+    },
+  },
+}));
 
 const QuoteComponent = () => {
-  return <div className="about">
-    <img src={qoute} alt={'Beauty of mathematics'} />
-  </div>;
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <img src={qoute} alt={"Beauty of mathematics"} />
+    </Card>
+  );
 };
 
 export default QuoteComponent;
