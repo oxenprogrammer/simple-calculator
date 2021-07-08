19af58e984ff04a1f15b62cdc5d05ac47890fe25
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-08 19:50:13 +0300

    Test(home): update test with describe

--- src/components/__test__/home.component.test.js
+++ src/components/__test__/home.component.test.js
@@ -2,8 +2,10 @@ import { render, screen } from "@testing-library/react";
 
 import HomeComponent from "../home.component";
 
-it("should have mathematics in home page", () => {
-  render(<HomeComponent />);
-  const mathematics = screen.getByText(/Mathematics/i);
-  expect(mathematics).toBeInTheDocument();
+describe("Test Home Component", () => {
+  it("should have mathematics in home page", () => {
+    render(<HomeComponent />);
+    const mathematics = screen.getByText(/Mathematics/i);
+    expect(mathematics).toBeInTheDocument();
+  });
 });
