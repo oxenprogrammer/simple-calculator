ded48773ddeeee3c2d38797c6080981fa8785cd3
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-08 21:52:48 +0300

    Test(page): add snapshot test

--- src/components/__test__/quote.component.test.js
+++ src/components/__test__/quote.component.test.js
@@ -1,14 +1,14 @@
 import Enzyme, { shallow } from "enzyme";
 
 import Adapter from "enzyme-adapter-react-16";
-import HomeComponent from "../home.component";
+import QuoteComponent from "../quote.component";
 import toJson from "enzyme-to-json";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("Home Component", () => {
+describe("Quote Component", () => {
   it("should render correctly", () => {
-    const wrapper = shallow(<HomeComponent />);
+    const wrapper = shallow(<QuoteComponent />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
